7787a9e4c5446f07024a66aea731eaccd28162f4
darkrider <darkrider@yandex-team.ru>
2022-04-14 18:15:18 +0300

    non_tree_data

--- test_tree_builder.py
+++ test_tree_builder.py
@@ -1,5 +1,18 @@
-from tree_builder import build_tree
+import pytest
+
+from tree_builder import build_tree, DataIsNotTree
 
 
 def test_build_empty_tree():
     assert build_tree(tuple()) == {}
+
+
+def test_non_tree():
+    source = (
+        (None, "1"),
+        ("1", "2"),
+        ("2", "1"),
+    )
+
+    with pytest.raises(DataIsNotTree):
+        build_tree(source)
--- tree_builder.py
+++ tree_builder.py
@@ -1,8 +1,26 @@
-from typing import Sequence, Optional, Tuple, Dict
+from typing import Sequence, Optional, Tuple, Dict, Set
 
 
 Node = Dict[str, "Node"]
 
 
+class TreeBuilderException(Exception):
+    ...
+
+
+class DataIsNotTree(TreeBuilderException):
+    ...
+
+
 def build_tree(data: Sequence[Tuple[Optional[str], str]]) -> Node:
+    nodes: Set[str] = set()
+
+    for from_node, to_node in data:
+        if to_node in nodes:
+            raise DataIsNotTree(
+                f"{to_node} is child already, data is not tree"
+            )
+        if from_node is not None:
+            nodes.add(from_node)
+
     return {}
